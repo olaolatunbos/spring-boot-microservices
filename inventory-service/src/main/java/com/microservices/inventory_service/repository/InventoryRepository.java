91fe704d7baa6021ad0110d0be56bb249d440365
Abdulquadri (Ola) Olatunbosun <olaolat@hotmail.com>
2024-08-25 18:57:17 +0100

    first commit

---
+++ inventory-service/src/main/java/com/microservices/inventory_service/repository/InventoryRepository.java
@@ -0,0 +1,9 @@
+package com.microservices.inventory_service.repository;
+
+
+import com.microservices.inventory_service.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    boolean existsBySkuCodeAndQuantityIsGreaterThanEqual(String skuCode, Integer quantity);
+}
